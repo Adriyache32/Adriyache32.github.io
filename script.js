7b6f76e95f9e2cd34e26590a6e78ed81e30dfe1e
Adriyache32 <adriyache32@users.noreply.github.com>
2026-06-23 14:49:15 -0400

    fix: actualizado link de cafecito

--- script.js
+++ script.js
@@ -1527,7 +1527,7 @@ function closeCoffee() {
 
 function coffeeYes() {
   closeCoffee();
-  window.open('https://cafecito.app/chanchosmc', '_blank');
+  window.open('https://cafecito.app/Adriyache32', '_blank');
 }
 
 function coffeeNo() {
